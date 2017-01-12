e6867bf01771ddead7ad515a1122316de3c4f6d2
djoctuk <djoctuk@yandex.ru>
2017-01-12 16:13:07 +0500

    update

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -9,7 +9,7 @@ class TestController extends Controller
 {
     public function index ()
     {
-        $tests = Tests::all();
+        $tests = Tests::latest('created_at')->limit(100)->get();
         foreach ($tests as $test)
         {
             echo $test->case_price . '<br>';
@@ -20,13 +20,31 @@ class TestController extends Controller
 
     public function store ()
     {
-
+        //$tests = Tests::->latest('created_at')->limit(3)->get();
         //$testy['case_type'] =  1;
         //$testy['case_price'] =  2;
+        $tests = Tests::latest('created_at')->limit(10)->get();
+
+        $array = array();
+
+        foreach ($tests as $test)
+        {
+            $array[] =  $test->case_price;
+        }
+
+
+        if (max($array) > 100)
+        {
+           $random_max = 100;
+        }
+        else $random_max = 200;
+
         Tests::create([
             'case_type' => 1,
-            'case_price' => random_int(20,200),
+            'case_price' => random_int(20,$random_max),
         ]);
+
         return redirect('/test');
+
     }
 }
