fff84262c66a65663d4262bf2258307cc4fffada
djoctuk <djoctuk@yandex.ru>
2017-01-13 15:28:30 +0500

    add

---
+++ app/Http/Controllers/FaucetController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FaucetController extends Controller
+{
+    public function index ()
+    {
+
+        echo '<a href="/test/create">hello people</a>';
+
+    }
+}
