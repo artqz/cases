2eec22ecf2628b9ba73b541fc5f138c91883e2cd
artqz <djoctuk@yandex.ru>
2017-03-09 10:39:19 +0500

    update

--- storage/framework/views/638320f783d43c5404aa868cf62a0afcbb9bd50a.php
+++ storage/framework/views/638320f783d43c5404aa868cf62a0afcbb9bd50a.php
@@ -3,7 +3,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
+                <div class="panel-heading">Регистрация</div>
                 <div class="panel-body">
                     <form class="form-horizontal" role="form" method="POST" action="<?php echo e(url('/register')); ?>">
                         <?php echo e(csrf_field()); ?>
@@ -16,7 +16,7 @@
                         <input id="ref_id" type="hidden" class="form-control" name="ref_id" value="<?php echo e($ref); ?>" required>
 
                         <div class="form-group<?php echo e($errors->has('name') ? ' has-error' : ''); ?>">
-                            <label for="name" class="col-md-4 control-label">Name</label>
+                            <label for="name" class="col-md-4 control-label">Имя</label>
 
                             <div class="col-md-6">
                                 <input id="name" type="text" class="form-control" name="name" value="<?php echo e(old('name')); ?>" required autofocus>
@@ -30,7 +30,7 @@
                         </div>
 
                         <div class="form-group<?php echo e($errors->has('email') ? ' has-error' : ''); ?>">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                            <label for="email" class="col-md-4 control-label">Эл. почта</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control" name="email" value="<?php echo e(old('email')); ?>" required>
@@ -44,7 +44,7 @@
                         </div>
 
                         <div class="form-group<?php echo e($errors->has('password') ? ' has-error' : ''); ?>">
-                            <label for="password" class="col-md-4 control-label">Password</label>
+                            <label for="password" class="col-md-4 control-label">Пароль</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control" name="password" required>
@@ -58,7 +58,7 @@
                         </div>
 
                         <div class="form-group<?php echo e($errors->has('password_confirmation') ? ' has-error' : ''); ?>">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
+                            <label for="password-confirm" class="col-md-4 control-label">Подтверждение</label>
 
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
@@ -70,11 +70,15 @@
                                 <?php endif; ?>
                             </div>
                         </div>
-
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <div class="g-recaptcha" data-sitekey="<?php echo e(env('RE_CAP_SITE')); ?>"></div>
+                            </div>
+                        </div>
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
-                                    Register
+                                    Регистрация
                                 </button>
                             </div>
                         </div>
