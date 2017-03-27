a3293f5ec33f61acc38f852edfdefc4da633efce
artqz <djoctuk@yandex.ru>
2017-03-27 16:21:58 +0500

    update

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -22,14 +22,7 @@ class UsersController extends Controller
             $items->user = $user;
             $items->clicks = Click::where('user_id', $user->id)->orderBy('created_at', 'desc')->limit(30)->get();
             $items->games = Game::where('user_id', $user->id)->orderBy('created_at', 'desc')->limit(30)->get();
-            $items->referrals_count = User::where('created_at', '<=', Carbon::now())
-                ->where('created_at', '>=', Carbon::now()->subWeek())
-                ->where('user_ref_id', '!=', 0)
-                ->where('confirm_email', '=', 1)
-                ->where('steamid', '!=', 0)
-                ->selectRaw('count(id) AS referrals, user_ref_id AS user_id')
-                ->selectRaw('count(user_ref_id)')
-                ->count();
+            $items->referrals_count = Referral::where('user_ref_id', $user->id)->count();
 
             return view('users.show', compact('items'));
         }
