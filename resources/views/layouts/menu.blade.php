3dc85be5bc173f72875c0d39766dc6646874127f
artqz <djoctuk@yandex.ru>
2017-03-30 10:24:25 +0500

    update

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,4 +1,4 @@
-<li><a href="{{ url('faucet') }}">Кликер @if(date("w",time()) == 0)<span style="color: red;">x2</span>@endif</a></li>
+<li><a href="{{ url('faucet') }}">Получить клики @if(date("w",time()) == 0)<span style="color: red;">x2</span>@endif</a></li>
 <li><a href="{{ url('shop') }}">Магазин</a></li>
 <li><a href="{{ url('rating') }}">Рейтинг</a></li>
 <li><a href="{{ url('discuss') }}">Общение</a></li>
