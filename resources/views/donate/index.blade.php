24552587d058137643dcc2b18808419e5045aa80
artqz <djoctuk@yandex.ru>
2017-04-01 13:26:15 +0500

    update

--- resources/views/donate/index.blade.php
+++ resources/views/donate/index.blade.php
@@ -5,10 +5,32 @@
 @section('content')
     <div>
         @include('layouts.flash')
-
-        <div><a href="{{ url('donate/buy/1') }}">Купить 1 кристалл</a></div>
-        <div><a href="{{ url('donate/buy/5') }}">Купить 5 кристаллов</a></div>
-        <div><a href="{{ url('donate/buy/10') }}">Купить 10 кристаллов</a></div>
+        <div class="crystals-list row">
+            <div class="col-sm-4">
+                <div class="crystal-card">
+                    <a href="{{ url('donate/buy/1') }}">
+                        <img src="{{ url('images/icons/clickcrystal.png') }}" alt="Кристалл">
+                        <div>Купить 1 кристалл</div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div  class="crystal-card">
+                    <a href="{{ url('donate/buy/5') }}">
+                        <img src="{{ url('images/icons/clickcrystal.png') }}" alt="Кристалл">
+                        <div>Купить 5 кристаллов</div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div  class="crystal-card">
+                    <a href="{{ url('donate/buy/10') }}">
+                        <img src="{{ url('images/icons/clickcrystal.png') }}" alt="Кристалл">
+                        <div>Купить 10 кристаллов</div>
+                    </a>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
 
