fcd1c6e6f58a4874b51e990b10e0d374bdb5cc0a
artqz <djoctuk@yandex.ru>
2017-04-01 10:43:12 +0500

    update

--- resources/views/donate/index.blade.php
+++ resources/views/donate/index.blade.php
@@ -6,9 +6,9 @@
     <div>
         @include('layouts.flash')
 
-        <div><a href="{{ url('donate/buy-one') }}">Купить 1 кристалл</a></div>
-        <div><a href="{{ url('donate/buy-five') }}">Купить 5 кристаллов</a></div>
-        <div><a href="{{ url('donate/buy-ten') }}">Купить 10 кристаллов</a></div>
+        <div><a href="{{ url('donate/buy/1') }}">Купить 1 кристалл</a></div>
+        <div><a href="{{ url('donate/buy/5') }}">Купить 5 кристаллов</a></div>
+        <div><a href="{{ url('donate/buy/10') }}">Купить 10 кристаллов</a></div>
     </div>
 @endsection
 
