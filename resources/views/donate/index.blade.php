8782f1d7f9cf10f80e98063be6955fe8f0a3b4e0
artqz <djoctuk@yandex.ru>
2017-04-01 16:50:17 +0500

    update

--- resources/views/donate/index.blade.php
+++ resources/views/donate/index.blade.php
@@ -7,28 +7,25 @@
         @include('layouts.flash')
         <div class="crystals-list row">
             <div class="col-sm-4">
-                <div class="crystal-card">
-                    <a href="{{ url('donate/buy/1') }}">
-                        <img src="{{ url('images/icons/clickcrystal.png') }}" alt="Кристалл">
-                        <div>Купить 1 кристалл</div>
-                    </a>
-                </div>
+                <a href="{{ url('donate/buy/1') }}" class="crystal-card">
+                   <img src="{{ url('images/icons/clickcrystal.png') }}" alt="Кристалл">
+                    <div class="crystals">Купить 1 кристалл</div>
+                    <div class="ruble">За 15 Рублей <span class="economy">экономия 0 рублей</span></div>
+                </a>
             </div>
             <div class="col-sm-4">
-                <div  class="crystal-card">
-                    <a href="{{ url('donate/buy/5') }}">
-                        <img src="{{ url('images/icons/clickcrystal.png') }}" alt="Кристалл">
-                        <div>Купить 5 кристаллов</div>
-                    </a>
-                </div>
+                <a href="{{ url('donate/buy/5') }}" class="crystal-card">
+                    <img src="{{ url('images/icons/clickcrystal.png') }}" alt="Кристалл">
+                    <div class="crystals">Купить 5 кристаллов</div>
+                    <div class="ruble">За 70 Рублей <span class="economy">экономия 5 рублей</span></div>
+                </a>
             </div>
             <div class="col-sm-4">
-                <div  class="crystal-card">
-                    <a href="{{ url('donate/buy/10') }}">
-                        <img src="{{ url('images/icons/clickcrystal.png') }}" alt="Кристалл">
-                        <div>Купить 10 кристаллов</div>
-                    </a>
-                </div>
+                <a href="{{ url('donate/buy/10') }}" class="crystal-card">
+                    <img src="{{ url('images/icons/clickcrystal.png') }}" alt="Кристалл">
+                    <div class="crystals">Купить 10 кристаллов</div>
+                    <div class="ruble">За 100 Рублей <span class="economy">экономия 50 рублей</span></div>
+                </a>
             </div>
         </div>
     </div>
