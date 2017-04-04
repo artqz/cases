25a00548f0f3df089e1195837019d2666e649817
artqz <djoctuk@yandex.ru>
2017-04-04 17:11:32 +0500

    update

--- app/Event.php
+++ app/Event.php
@@ -7,7 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Event extends Model
 {
     protected $fillable = [
-        'text', 'value', 'user_id', 'status', 'image',
+        'text',
+        'value',
+        'user_id',
+        'status',
+        'image',
+        'type',
+        'data',
+        'url',
     ];
 
     public function user()
