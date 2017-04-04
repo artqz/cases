5a167d554303895926c0cac52b536feb1b5c1cc5
artqz <djoctuk@yandex.ru>
2017-04-04 16:34:44 +0500

    update

--- resources/views/widgets/events.blade.php
+++ resources/views/widgets/events.blade.php
@@ -1,11 +1,16 @@
-@foreach($events as $event)
-    <li>
-        <a class="event-link" href="{{ url('events') }}">
-            <img src="{{ $event->image }}" alt="{{ $event->text }}">
-            <div class="content">
-                <div class="text">{{ $event->text }}</div>
-                <div class="date">{{ $event->created_at->diffForHumans() }}</div>
-            </div>
-        </a>
-    </li>
-@endforeach
+Уведомления
+@if($events)
+    @foreach($events as $event)
+        <li>
+            <a class="event-link" href="{{ url('events') }}">
+                <img src="{{ $event->image }}" alt="{{ $event->text }}">
+                <div class="content">
+                    <div class="text">{{ $event->text }}</div>
+                    <div class="date">{{ $event->created_at->diffForHumans() }}</div>
+                </div>
+            </a>
+        </li>
+    @endforeach
+@else
+    <li><a href="#">Уведомлений пока нет!</a></li>
+@endif
