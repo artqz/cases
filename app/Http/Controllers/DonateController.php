dd148bb2746d69aff9a4827f7ff93a1100ee3983
artqz <djoctuk@yandex.ru>
2017-04-05 17:35:20 +0500

    update

--- app/Http/Controllers/DonateController.php
+++ app/Http/Controllers/DonateController.php
@@ -43,7 +43,7 @@ class DonateController extends Controller
 
         if ($order) {
             $payment = new \Idma\Robokassa\Payment(
-                'steamclicks.ru', 'Artem110789', 'Kuznetsov110789', true
+                'steamclicks.ru', 'd2zPEddL', 'Art!!0789Kuz', false
             );
 
             $payment
@@ -59,7 +59,7 @@ class DonateController extends Controller
     public function success () {
 
         $payment = new \Idma\Robokassa\Payment(
-            'steamclicks.ru', 'Artem110789', 'Kuznetsov110789', true
+            'steamclicks.ru', 'd2zPEddL', 'Art!!0789Kuz', false
         );
 
             if ($payment->validateSuccess($_GET)) {
@@ -78,7 +78,7 @@ class DonateController extends Controller
 
     public function result () {
         $payment = new \Idma\Robokassa\Payment(
-            'steamclicks.ru', 'Artem110789', 'Kuznetsov110789', true
+            'steamclicks.ru', 'd2zPEddL', 'Art!!0789Kuz', false
         );
 
         if ($payment->validateResult($_GET)) {
