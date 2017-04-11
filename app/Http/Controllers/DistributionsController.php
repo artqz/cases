eb91dbfd37ed9d7fb333eccfc9ae79a7c7d6047f
artqz <djoctuk@yandex.ru>
2017-04-11 14:42:15 +0500

    update

--- app/Http/Controllers/DistributionsController.php
+++ app/Http/Controllers/DistributionsController.php
@@ -22,15 +22,15 @@ class DistributionsController extends Controller
         if ($type == 'premium') {
             $distributions = Distribution::where('level', 2)
                 ->where('status', 0)
-                ->orderBy('created_at', 'desc')
+                ->orderBy('topped_at', 'desc')
                 ->paginate(30);
         } elseif ($type == 'active') {
             $distributions = Distribution::where('status', 0)
-                ->orderBy('created_at', 'desc')
+                ->orderBy('topped_at', 'desc')
                 ->paginate(30);
         } elseif ($type == 'passed') {
             $distributions = Distribution::where('status', 1)->orWhere('status', 2)
-                ->orderBy('created_at', 'desc')
+                ->orderBy('topped_at', 'desc')
                 ->paginate(30);
         } else {
             $distributions = Distribution::orderBy('topped_at', 'desc')
