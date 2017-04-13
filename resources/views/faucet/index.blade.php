2857a6a13001b1dfdd4c0887e0883b9fd75ee4e4
artqz <djoctuk@yandex.ru>
2017-04-13 21:45:19 +0500

    update

--- resources/views/faucet/index.blade.php
+++ resources/views/faucet/index.blade.php
@@ -17,16 +17,21 @@
             </form>
         </div>
         <div class="col-sm-6 col-md-6">
-            <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-            <!— Steamclicks.ru —>
-            <ins class="adsbygoogle"
-                 style="display:block"
-                 data-ad-client="ca-pub-1983162371188961"
-                 data-ad-slot="2849935534"
-                 data-ad-format="auto"></ins>
-            <script>
-                (adsbygoogle = window.adsbygoogle || []).push({});
-            </script>
+            <script type='text/javascript'>(function() {
+                    /* Optional settings (these lines can be removed): */
+                    subID = "";  // - local banner key;
+                    injectTo = "";  // - #id of html element (ex., "top-banner").
+                    /* End settings block */
+
+                    if(injectTo=="")injectTo="admitad_shuffle"+subID+Math.round(Math.random()*100000000);
+                    if(subID=='')subid_block=''; else subid_block='subid/'+subID+'/';
+                    document.write('<div id="'+injectTo+'"></div>');
+                    var s = document.createElement('script');
+                    s.type = 'text/javascript'; s.async = true;
+                    s.src = 'https://ad.admitad.com/shuffle/feccab2e4e/'+subid_block+'?inject_to='+injectTo;
+                    var x = document.getElementsByTagName('script')[0];
+                    x.parentNode.insertBefore(s, x);
+                })();</script>
         </div>
     </div>
 @endsection
--- resources/views/pages/start.blade.php
+++ resources/views/pages/start.blade.php
@@ -55,27 +55,21 @@
         <br>
         <div class="container">
             <div class="col-sm-4">
-                <!-- Yandex.RTB R-A-217702-3 -->
-                <div id="yandex_rtb_R-A-217702-3"></div>
-                <script type="text/javascript">
-                    (function(w, d, n, s, t) {
-                        w[n] = w[n] || [];
-                        w[n].push(function() {
-                            Ya.Context.AdvManager.render({
-                                blockId: "R-A-217702-3",
-                                renderTo: "yandex_rtb_R-A-217702-3",
-                                horizontalAlign: false,
-                                async: true
-                            });
-                        });
-                        t = d.getElementsByTagName("script")[0];
-                        s = d.createElement("script");
-                        s.type = "text/javascript";
-                        s.src = "//an.yandex.ru/system/context.js";
-                        s.async = true;
-                        t.parentNode.insertBefore(s, t);
-                    })(this, this.document, "yandexContextAsyncCallbacks");
-                </script>
+                <script type='text/javascript'>(function() {
+                        /* Optional settings (these lines can be removed): */
+                        subID = "";  // - local banner key;
+                        injectTo = "";  // - #id of html element (ex., "top-banner").
+                        /* End settings block */
+
+                        if(injectTo=="")injectTo="admitad_shuffle"+subID+Math.round(Math.random()*100000000);
+                        if(subID=='')subid_block=''; else subid_block='subid/'+subID+'/';
+                        document.write('<div id="'+injectTo+'"></div>');
+                        var s = document.createElement('script');
+                        s.type = 'text/javascript'; s.async = true;
+                        s.src = 'https://ad.admitad.com/shuffle/feccab2e4e/'+subid_block+'?inject_to='+injectTo;
+                        var x = document.getElementsByTagName('script')[0];
+                        x.parentNode.insertBefore(s, x);
+                    })();</script>
             </div>
             <div class="col-sm-4">
                     <!-- Yandex.RTB R-A-217702-4 -->
