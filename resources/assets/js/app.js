f781fab7e54b3c8dd7fbc5ff77e1850d6ef95fcb
artqz <djoctuk@yandex.ru>
2017-04-19 15:31:06 +0500

    update

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -9,7 +9,7 @@ require('./bootstrap');
 require('./jquery.countdown-2.2.0/jquery.countdown');
 
 /**
- * Next, we will create a fresh Vue application instance and attach it to
+ * 1Next, we will create a fresh Vue application instance and attach it to
  * the body of the page. From here, you may begin adding components to
  * the application, or feel free to tweak this setup for your needs.
  */
@@ -19,6 +19,7 @@ import Inventory from './components/Inventory.vue';
 new Vue({
     el: '#app-vue',
     components: { Inventory },
+
 });
 
 /*
