8b7474118ecc2a56d2aed89623cdc58349fb35d7
artqz <djoctuk@yandex.ru>
2017-06-08 11:35:38 +0500

    update

--- resources/views/widgets/reklama.blade.php
+++ resources/views/widgets/reklama.blade.php
@@ -1,1 +1,3 @@
 <br>
+<iframe data-aa='542706' src='//ad.a-ads.com/542706?size=120x600' scrolling='no' style='width:120px; height:600px; border:0px; padding:0;overflow:hidden' allowtransparency='true'></iframe>
+<br>
